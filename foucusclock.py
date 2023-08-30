9e8f27ae86a6995aae30bb8d55e529853a566931
ALAN54440 <114839936+ALAN54440@users.noreply.github.com>
2023-08-30 23:44:45 +0800

    Create foucusclock.py

---
+++ foucusclock.py
@@ -0,0 +1,14 @@
+import time
+
+def focus_timer(minutes):
+    seconds = minutes * 60
+    while seconds > 0:
+        mins, secs = divmod(seconds, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        seconds -= 1
+    print("Time's up! Stay focused!")
+
+focus_time = int(input("请输入专注时长（分钟）："))
+focus_timer(focus_time)
